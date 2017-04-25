b1b255dc7d07bcecf1390f398d6162d6a39eb146
sachinss <sachinss@IND-MHPDW100124>
2017-04-25 11:01:21 +0530

    latest changes

--- TFS/TFSMerger.cs
+++ TFS/TFSMerger.cs
@@ -3,11 +3,26 @@ using CommonLibrary;
 
 using Microsoft.TeamFoundation.Client;
 using CodeMergerEntity;
+using Microsoft.TeamFoundation.VersionControl.Client;
 
 namespace TFS
 {
     public class TFSMerger : IMerger
     {
+        private const string TfsServer = "http://localhost:8080/tfs";
+        private const string TfsUserName = "tfsusername";
+        private const string TfsPassword = "tfspassword";
+        private const string FromBranch = "$/TestSource/Dev";
+        private const string ToBranch = "$/TestSource/Main";
+
+        static TfsTeamProjectCollection tfsTeamProjectCollection;
+        static TfsTeamProjectCollection GetTeamProjectCollection()
+        {
+            var tfsTeamProjectCollection = new TfsTeamProjectCollection(new Uri(TfsServer), new System.Net.NetworkCredential(TfsUserName, TfsPassword));
+            tfsTeamProjectCollection.EnsureAuthenticated();
+            return tfsTeamProjectCollection;
+        }
+
         ConfigEntity configEntity;
 
         public TFSMerger(ConfigEntity config)
@@ -17,10 +32,10 @@ namespace TFS
 
         public void Merge()
         {
-            throw new NotImplementedException();
+            tfsTeamProjectCollection = GetTeamProjectCollection();
+            var versionControl = tfsTeamProjectCollection.GetService<VersionControlServer>();
+            var mergeCandidates = versionControl.GetMergeCandidates(FromBranch, ToBranch, RecursionType.Full);
 
-            
-            
         }
     }
 }
